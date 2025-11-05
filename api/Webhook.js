1775c4b634a5baae99bea1e967c4a93691d19ce2
Damien Warren <your-email@example.com>
2025-11-05 00:42:37 +0000

    basic handler

--- api/Webhook.js
+++ api/Webhook.js
@@ -1,20 +1,3 @@
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    let rawBody = '';
-    req.on('data', chunk => {
-      rawBody += chunk;
-    });
-    req.on('end', () => {
-      const body = JSON.parse(rawBody || '{}');
-      res.status(200).json({ echo: body });
-    });
-  } else {
-    res.status(405).json({ message: "Method not allowed" });
-  }
+  res.status(200).json({ message: "Webhook works" });
 }
